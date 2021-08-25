90e0f53c0a23309e88847d75bac20e63b76264d8
marlon felipe martins de oliveira <marlon@pop-os.localdomain>
2021-08-25 13:57:06 -0300

    add Header in homeScreeen

--- screens/Home.js
+++ screens/Home.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, TouchableOpacity, Image, TextInput } from 'react-native';
 
 import { styles } from './Styles/HomeStyle';
 
+
 export default class Home extends Component {
-    constructor(){
+    constructor( { navigation }){
         super();
         this.state = {
+            navigation: navigation,
 
         }
+        
+         
     }
+
 
     render(){
         return (
             <View style={styles.container}>
-              <Text>Home</Text>
+                <View style={styles.header}>
+                    <View>
+                        <TextInput style={styles.headerInput} placeholder="Buscar" />
+                    </View>
+                    <View style={styles.headerIcon}>
+                        <TouchableOpacity>
+                            <Image 
+                                style={{width: 27, height: 27}}
+                                source={require('../assets/carrinho-de-compras.png')}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                  
+                </View>
+
+
+
+
+
             </View>
         );
     }
--- screens/Styles/HomeStyle.js
+++ screens/Styles/HomeStyle.js
@@ -3,8 +3,33 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      backgroundColor: 'white',
+      //alignItems: 'center',
+      //justifyContent: 'center',
     },
+    header: {
+      flexDirection: "row",
+      alignItems: 'flex-end',
+      justifyContent: "center",
+      backgroundColor: 'gray',
+      height: 70,
+    },
+    headerInput: {
+      borderWidth: 2,
+      borderRadius: 100,
+      minWidth: 300,
+      maxWidth: 320,
+      minHeight: 34,
+      backgroundColor: 'white',
+      marginBottom: 10,
+      paddingLeft: 10,
+      paddingRight: 10
+    },
+    headerIcon: {
+      marginBottom: 10,
+      marginLeft: 4
+
+    }
+
+    
 });
